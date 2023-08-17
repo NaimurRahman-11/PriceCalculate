3e01c8b92c67fe3e70fb9bf82f07b346f33e1a5e
Naimur Rahman <nirzon.naim@gmail.com>
2023-08-17 10:23:24 -0400

    Input Validation

--- index.js
+++ index.js
@@ -18,10 +18,12 @@ function addRow() {
     clone.querySelector(".paid-input").addEventListener("input", updateDueAmount);
 }
 
-
 function updateDueAmount() {
     const totalPrice = parseFloat(document.getElementById("totalPrice").value) || 0;
-    const paidAmount = parseFloat(document.getElementById("paid").value) || 0;
+    let paidAmount = parseFloat(document.getElementById("paid").value) || 0;
+
+    // Ensure paid amount doesn't exceed total price
+    paidAmount = Math.min(paidAmount, totalPrice);
 
     const dueAmount = totalPrice - paidAmount;
 
@@ -33,21 +35,22 @@ function updateDueAmount() {
 const paidInput = document.getElementById("paid");
 paidInput.addEventListener("input", updateDueAmount);
 
-
-
 function updateTotalPrice() {
     const formRows = document.querySelectorAll(".form-row");
     let totalPrice = 0;
 
     formRows.forEach(row => {
-        const quantity = parseFloat(row.querySelector(".quantity-input").value);
-        const price = parseFloat(row.querySelector(".price-input").value);
-        totalPrice += quantity * price;
+        const quantity = parseFloat(row.querySelector(".quantity-input").value) || 0;
+        const price = parseFloat(row.querySelector(".price-input").value) || 0;
+        totalPrice += Math.max(0, quantity) * Math.max(0, price); // Ensure positive values
     });
 
     // Update the "Total Price" input field
     const totalPriceInput = document.getElementById("totalPrice");
     totalPriceInput.value = totalPrice.toFixed(2); // Display total with 2 decimal places
+
+    // Update due amount
+    updateDueAmount();
 }
 
 function initializeListeners() {
@@ -60,3 +63,57 @@ function initializeListeners() {
 
 // Call initializeListeners() once to attach listeners to initial input fields
 initializeListeners();
+
+
+// Attach event listener to the "Submit" button
+const submitButton = document.getElementById("submitButton");
+submitButton.addEventListener("click", displaySummary);
+
+function displaySummary() {
+    const productSelects = document.querySelectorAll(".product-select");
+    const quantities = document.querySelectorAll(".quantity-input");
+    const summaryDiv = document.getElementById("summaryAlert");
+    
+    let summaryContent = `
+        <div class="alert alert-success alert-dismissible fade show" role="alert">
+            <h4 class="alert-heading">Goods Summary</h4>
+    `;
+
+    for (let i = 0; i < productSelects.length; i++) {
+        const selectedProduct = productSelects[i].value;
+        const quantity = parseFloat(quantities[i].value) || 0;
+
+        if (selectedProduct !== "Select Your Goods" && quantity > 0) {
+            summaryContent += `
+                <p><strong>Product:</strong> ${selectedProduct}</p>
+                <p><strong>Quantity:</strong> ${quantity}</p>
+                <hr>
+            `;
+        }
+    }
+
+    const totalPrice = parseFloat(document.getElementById("totalPrice").value) || 0;
+    const paidAmount = parseFloat(document.getElementById("paid").value) || 0;
+    const dueAmount = parseFloat(document.getElementById("due").value) || 0;
+    const selectedDate = document.getElementById("date").value;
+
+    summaryContent += `
+            <p><strong>Total Price:</strong> BDT. ${totalPrice.toFixed(2)}</p>
+            <p><strong>Paid Amount:</strong> BDT. ${paidAmount.toFixed(2)}</p>
+            <p><strong>Due Amount:</strong> BDT. ${dueAmount.toFixed(2)}</p>
+            <p><strong>Date:</strong> ${selectedDate}</p>
+            <hr>
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="alert" aria-label="Close">
+                Close
+            </button>
+        </div>
+    `;
+
+    summaryDiv.innerHTML = summaryContent;
+
+    // Show the summary alert
+    summaryDiv.style.display = "block";
+}
+
+
+
